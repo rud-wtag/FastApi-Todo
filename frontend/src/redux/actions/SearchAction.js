df71a495b74c74186ac77e1c62f54c3c9fe56ccc
rud-wtag <127865276+rud-wtag@users.noreply.github.com>
2024-05-16 19:15:21 +0600

    Worked on search

--- frontend/src/redux/actions/SearchAction.js
+++ frontend/src/redux/actions/SearchAction.js
@@ -1,9 +1,24 @@
 import { searchActionTypes } from 'redux/constants/ActionTypes';
+import axios from 'axios'
+import { toast } from './TodoAction';
+import {TOAST_TYPE_ERROR} from '../../utils/constants/index'
 
 export const setSearch = (query) => {
-  return {
-    type: searchActionTypes.SET_SEARCH,
-    payload: query
+  return async (dispatch) => {
+    axios
+      .get(`/tasks?search_query=${query}`)
+      .then((res) => {
+        if (res.status == 200)
+          dispatch({
+            type: searchActionTypes.SET_SEARCH,
+            payload: res.data.items
+          });
+        dispatch(toast({ type: 'success', message: 'task completed' }));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(toast({ type: TOAST_TYPE_ERROR, message: 'Failed to update task' }));
+      });
   };
 };
 
